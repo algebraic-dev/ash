fa46fab9feee87e2b5cd3573db3441602d1e71c7
Sofia Rodrigues <sofia@algebraic.dev>
2023-08-21 10:57:35 -0300

    chore: update lakefile to 2023-02-22 version and update dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -5,5 +5,5 @@ package ash
 
 lean_lib Ash
 
-require soda from "../Soda"
-require socket from git "https://github.com/xubaiw/lean4-socket.git"@"3ad658b2c3f659699861f75372a8b316bf0e1c92"
+require soda from git "git@github.com:algebraic-sofia/soda.git"
+require melp from git "git@github.com:algebraic-sofia/melp.git"
