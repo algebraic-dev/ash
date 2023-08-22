9e26fe719333b07f17fd2c4cff54327d33c64601
Sofia Rodrigues <sofia@algebraic.dev>
2023-08-21 22:55:07 -0300

    chore: changes ssh links to https

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,6 @@ package ash
 
 lean_lib Ash
 
-require lina from git "git@github.com:algebraic-sofia/lina.git"
-require soda from git "git@github.com:algebraic-sofia/soda.git"
-require melp from git "git@github.com:algebraic-sofia/melp.git"
+require lina from git "https://github.com/algebraic-sofia/lina.git"
+require soda from git "https://github.com/algebraic-sofia/soda.git"
+require melp from git "https://github.com/algebraic-sofia/melp.git"
